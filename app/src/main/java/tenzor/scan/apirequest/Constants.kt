8da9728c661dd87d8fdb8e7141595c8688e67cff
Matej Jazbinšek <jazbinsek@gmail.com>
2023-04-21 13:17:38 +0200

    Constant class renaming of Constants.kt

--- app/src/main/java/tenzor/scan/apirequest/Constants.kt
+++ app/src/main/java/tenzor/scan/apirequest/Constants.kt
@@ -2,11 +2,12 @@ package tenzor.scan.apirequest
 
 const val CONST_JSON = "application/json; charset=utf-8"
 
+
 class ApiData{
     companion object{
-        const val DATA_TYPE_QR = "QR"
-        const val DATA_TYPE_BAR = "BAR"
-        const val DATA_TYPE_MIFARE = "MIFARE"
-        const val DATA_TYPE_NFC = "NFC"
+        const val CONST_DATA_TYPE_QR = "QR"
+        const val CONST_DATA_TYPE_BAR = "BAR"
+        const val CONST_DATA_TYPE_MIFARE = "MIFARE"
+        const val CONST_DATA_TYPE_NFC = "NFC"
     }
 }
